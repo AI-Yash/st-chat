0455ff7ede278797c244e5b020fe8eb791494a41
Yash Pawar <yashpawarp@gmail.com>
2021-12-23 07:03:39 +0530

    some progress ☺

--- streamlit_chat/frontend/src/index.tsx
+++ streamlit_chat/frontend/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import MyComponent from "./MyComponent"
+import Chat from "./stChat"
 
 ReactDOM.render(
   <React.StrictMode>
-    <MyComponent />
+    <Chat />
   </React.StrictMode>,
   document.getElementById("root")
 )
--- streamlit_chat/frontend/src/stChat.tsx
+++ streamlit_chat/frontend/src/stChat.tsx
@@ -1,13 +1,26 @@
 import {
+  // eslint-disable-next-line 
   Streamlit,
   StreamlitComponentBase,
   withStreamlitConnection,
 } from "streamlit-component-lib"
 import React, { ReactNode } from "react"
-
+import "./index.css"
 class Chat extends StreamlitComponentBase {
   public render = (): ReactNode => {
-    return "Hello WOrld"
+    // eslint-disable-next-line 
+    let isUser = this.props.args["is_user"]
+    let pfpSeed = this.props.args["seed"]
+    let avatarUrl = `https://avatars.dicebear.com/api/bottts/${pfpSeed}.svg`
+
+    return (
+      <div className="chat">
+        <img src={avatarUrl} alt="profile" />
+        <div>
+          {this.props.args["message"]}
+        </div>
+      </div>
+    )
   }
 }
 

847c039325d55d62346462aef11b1eda3dfca7cb
Yash Pawar <yashpawarp@gmail.com>
2021-12-26 04:45:03 +0530

    add build

---
+++ streamlit_chat/frontend/build/precache-manifest.8c9cce7687b5a850111a8e105fa91af3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7be2d32d7414e9721665389c36e57b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "badb402b4acdef8a2f9d",
+    "url": "./static/js/2.382cf88c.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.382cf88c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80faf02cf8bac59266d0",
+    "url": "./static/js/main.8224ed41.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);

f7065e491ce2e2f492689f317feef2292d2e6d84
Yash Pawar <yashpawarp@gmail.com>
2021-12-25 12:10:27 +0530

    use streamlit theme

--- streamlit_chat/frontend/src/stChat.tsx
+++ streamlit_chat/frontend/src/stChat.tsx
@@ -15,11 +15,26 @@ class Chat extends StreamlitComponentBase {
     let avatarUrl = `https://avatars.dicebear.com/api/${pfpType}/${pfpSeed}.svg`
     let classes = isUser ? "chat user" : "chat"
 
+    // Streamlit sends us a theme object via props that we can use to ensure
+    // that our component has visuals that match the active theme in a
+    // streamlit app.
+    const { theme } = this.props
+    const messageBoxStyle: React.CSSProperties = {}
+
+    // Maintain compatibility with older versions of Streamlit that don't send
+    // a theme object.
+    if (theme) {
+      // Use the theme object to style our button border. Alternatively, the
+      // theme style is defined in CSS vars.
+      // messageBoxStyle.border = `1px solid ${theme.primaryColor}`
+      messageBoxStyle.backgroundColor = theme.secondaryBackgroundColor 
+    }
+
     return (
       <div className={classes}>
         <img src={avatarUrl} alt="profile" draggable="false"/>
 
-        <div className="message">
+        <div className="message" style={messageBoxStyle}>
           {this.props.args["message"]}
         </div>
       </div>

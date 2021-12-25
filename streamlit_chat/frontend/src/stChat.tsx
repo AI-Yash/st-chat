71d5e95472fa1aaf12b748932b923b29ac0dbe2a
Yash Pawar <yashpawarp@gmail.com>
2021-12-25 15:33:28 +0530

    replace CSS with emotion.js 🎉🎉🎉🎉

--- streamlit_chat/frontend/src/stChat.tsx
+++ streamlit_chat/frontend/src/stChat.tsx
@@ -5,34 +5,71 @@ import {
   withStreamlitConnection,
 } from "streamlit-component-lib"
 import React, { ReactNode } from "react"
-import "./index.css"
+import styled from '@emotion/styled'
+import { css } from '@emotion/react'
+
+
+const specificChatDivStyles = (props: {isUser: boolean}) => {
+  if (props.isUser) {
+    return css`
+      flex-direction: row-reverse;
+      & > div {
+        text-align: right;
+      }
+    `
+  }
+  return css``
+}
 
 class Chat extends StreamlitComponentBase {
   public render = (): ReactNode => {
-    const { isUser, avatarStyle, seed } = this.props.args;
-    let avatarUrl = `https://avatars.dicebear.com/api/${avatarStyle}/${seed}.svg`
-    let classes = isUser ? "chat user" : "chat"
-
+    const { isUser, avatarStyle, seed, message } = this.props.args;
+    const avatarUrl = `https://avatars.dicebear.com/api/${avatarStyle}/${seed}.svg`
+    
     // Streamlit sends us a theme object via props that we can use to ensure
     // that our component has visuals that match the active theme in a
     // streamlit app.
     const { theme } = this.props
-    const messageBoxStyle: React.CSSProperties = {}
-
+    
     // Maintain compatibility with older versions of Streamlit that don't send
     // a theme object.
-    if (theme) {
-      messageBoxStyle.backgroundColor = theme.secondaryBackgroundColor 
+    if (!theme) {
+      return <div>Theme is undefined, please check streamlit version.</div>
     }
 
-    return (
-      <div className={classes}>
-        <img src={avatarUrl} alt="profile" draggable="false"/>
+    const Chat = styled.div`
+      width: 100%;
+      height: auto;
+      display: flex;
+      flex-direction: row;
+      font-family: ${theme.font}, 'Segoe UI', 'Roboto', sans-serif;
+      margin: 0;
+      ${specificChatDivStyles}
+    `
 
-        <div className="message" style={messageBoxStyle}>
-          {this.props.args["message"]}
-        </div>
-      </div>
+    const Avatar = styled.img`
+      border: 1px solid transparent;
+      border-radius: 50%;
+      height: 3rem;
+      width: 3rem;
+      margin: 0;
+    `
+
+    const Message = styled.div`
+      display: inline-block;
+      background: ${theme.secondaryBackgroundColor};
+      border: 1px solid transparent;
+      border-radius: 10px;
+      padding: 10px 14px;
+      margin: 5px 20px;
+      max-width: 70%;
+    `
+
+    return (
+      <Chat isUser={isUser}>
+        <Avatar src={avatarUrl} alt="profile" draggable="false"/>
+        <Message>{message}</Message>
+      </Chat>
     )
   }
 }

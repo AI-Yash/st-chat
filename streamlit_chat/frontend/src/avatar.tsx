7c9849537a72fe891e8a4c4bfa04b71aa480e62c
Yash Pawar <yashpawarp@gmail.com>
2023-06-25 22:31:34 +0530

    v0.1.1
    * make links open in new tab
    * use css instead of emotion.js
    * add ability to remove avatar

--- streamlit_chat/frontend/src/avatar.tsx
+++ streamlit_chat/frontend/src/avatar.tsx
@@ -1,6 +1,6 @@
-// import React from 'react'
+import { ReactElement } from "react"
 
-function Avatar(props: {src: string}) {
+function Avatar(props: {src: string}): ReactElement {
   return (
      <div className='avatar'>
         <img src={props.src} alt="profile" draggable="false" />
--- streamlit_chat/frontend/src/stChat.tsx
+++ streamlit_chat/frontend/src/stChat.tsx
@@ -5,25 +5,19 @@ import {
   withStreamlitConnection,
 } from "streamlit-component-lib"
 import { ReactNode } from "react"
-import styled from '@emotion/styled'
-import { css } from '@emotion/react'
-
 
 import './stChat.css'
 import Avatar from "./avatar"
 import Message from "./message"
+import ChatElement from './Chat.tsx'
 
 class Chat extends StreamlitComponentBase {
-  public componentDidMount() {
-    let body = document.querySelector('body');
-    body?.style.setProperty('--md-primary-color', '#00FF00');
-  }
   public render = (): ReactNode => {
     Streamlit.setFrameHeight(window.innerHeight)
+
     // const { isUser, avatarStyle, seed, message, logo } = this.props.args;
     const { isUser, avatarStyle, seed, message, logo, allow_html, is_table } = this.props.args;
     const avatarUrl: string = !!logo ? logo: `https://api.dicebear.com/5.x/${avatarStyle}/svg?seed=${seed}`
-    
     
     // Streamlit sends us a theme object via props that we can use to ensure
     // that our component has visuals that match the active theme in a
@@ -35,44 +29,21 @@ class Chat extends StreamlitComponentBase {
     if (!theme) {
       return <div>Theme is undefined, please check streamlit version.</div>
     }
-    
-    // styles for the avatar image
-    // const Avatar = styled.img({
-    //   border: `1px solid transparent`,
-    //   borderRadius: '50%',
-    //   height: '3rem',
-    //   width: '3rem',
-    //   margin: 0
-    // })
-       
-    // styles for the container
-    const Chat = styled.div({
-      display: 'flex',
-      // flexDirection: 'row',
-      fontFamily: `${theme.font}, 'Segoe UI', 'Roboto', sans-serif`, 
-      height: 'auto',
-      margin: 0,
-      width: '100%'
-    }, 
-    (props: {isUser: boolean}) => {  // specific styles
-      if (props.isUser){
-        return css`
-          flex-direction: row-reverse;
-          & > div {
-            text-align: right;
-          }
-        `
-      }
-      return css``
-    })
-
+  
+    // set CSS variables for theme
+    let body = document.querySelector('body')
+    body?.style.setProperty('--secondary-bg-color', theme.secondaryBackgroundColor)
+    body?.style.setProperty('--bg-color', theme.backgroundColor)
+    body?.style.setProperty('--primary-color', theme.primaryColor)
+    body?.style.setProperty('--text-color', theme.textColor)
+    body?.style.setProperty('--font', theme.font)
     
 
     return (
-      <Chat isUser={isUser}>
+      <ChatElement isUser={isUser} avatar={!(avatarStyle === 'no-avatar')}>
         <Avatar src={avatarUrl}/>
         <Message is_table={is_table} message={message} allow_html={allow_html}/>
-      </Chat>
+      </ChatElement>
     )
   }
 }
